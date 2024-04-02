13c688745ddd4a857068207f48fc9c1f750d3ba7
Victor Teo <victor.teo@fillmula.com>
2024-04-02 11:02:16 +0800

    select arrow element

---
+++ src/components/generated/select/SelectScrollArrowElement.tsx
@@ -0,0 +1,18 @@
+import { styled } from "@linaria/react"
+
+const SelectScrollArrowElement = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 0;
+    cursor: default;
+    font-size: 12px;
+    line-height: 1;
+    height: 25px;
+    user-select: none;
+    position: absolute;
+    z-index: 1;
+    width: 100%;
+`
+
+export default SelectScrollArrowElement
