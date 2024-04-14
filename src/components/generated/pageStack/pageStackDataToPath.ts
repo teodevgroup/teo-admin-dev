f0763b2ce148aa47539058f856e87bf067cedb5a
Victor Teo <victor.teo@fillmula.com>
2024-04-14 23:43:12 +0800

    save for tomorrow

---
+++ src/components/generated/pageStack/pageStackDataToPath.ts
@@ -0,0 +1,11 @@
+import { fromQuery } from 'query-string-parser'
+import { PageStackData } from "./PageStackData"
+import { PageStackItem } from "./PageStackItem"
+import { PageStackItemKey } from '../../extended/pageStack/PageStackItemKeys'
+
+const pageStackDataToPath: (stack: PageStackData) => string = (stack: PageStackData) => {
+    
+    return ""
+}
+
+export default pageStackDataToPath
