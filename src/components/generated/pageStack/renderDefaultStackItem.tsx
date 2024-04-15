d2cda486c1d7ecc8cd5f6eaaabcae8685b3bdbdd
Victor Teo <victor.teo@fillmula.com>
2024-04-15 12:51:54 +0800

    start today

--- src/components/generated/pageStack/renderDefaultStackItem.tsx
+++ src/components/generated/pageStack/renderDefaultStackItem.tsx
@@ -6,7 +6,12 @@ import Button from '../../extended/button/Button'
 export default function renderDefaultStackItem(item: PageStackItem): ReactElement | undefined {
     switch (item.key) {
         case "Admin":
-            return <div key="Admin" style={{background: "white", paddingBottom: "20px"}}>
+            return <div key="Admin" style={{
+                background: "white", 
+                paddingBottom: "20px",
+                margin: "20px",
+                borderRadius: 10,
+            }}>
                 <Input /><Button>Button</Button>
             </div>
         case "User":
