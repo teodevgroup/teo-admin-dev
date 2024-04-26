a00a300cf0478088597335d8fcb1c75036a07c83
Victor Teo <victor.teo@fillmula.com>
2024-04-27 01:39:54 +0800

    correct jump to edit page

--- src/components/generated/pages/Admin/RecordsList.tsx
+++ src/components/generated/pages/Admin/RecordsList.tsx
@@ -3,20 +3,34 @@ import RecordsContainer from '../../records/RecordsContainer'
 import { TableVirtuoso } from 'react-virtuoso'
 import { suspend } from 'suspend-react'
 import { teo } from '../../../../lib/generated/teo'
+import { pick } from 'radash'
+import usePageStackPage from '../../pageStack/usePageStackPage'
 
 type RecordsListProps = {
     filter: any
 }
 
 const RecordsList = ({ filter }: RecordsListProps) => {
+    const { pushStack } = usePageStackPage()
     const { data, meta } = suspend(async () => {
         return await teo.admin.findMany(filter)
     }, [filter])
     return <RecordsContainer>
         <TableVirtuoso
-            components={{TableHead: forwardRef(({ style, ...props }, ref) => {
-                return <thead {...props} ref={ref} />
-            })}}
+            components={{
+                TableHead: forwardRef(({ style, ...props }, ref) => {
+                    return <thead {...props} ref={ref} />
+                }),
+                TableRow: ({ ...props }) => {
+                    return <tr {...props} onClick={() => {
+                        const item = data[props["data-index"]]
+                        pushStack({
+                            key: "AdminForm",
+                            query: pick(item, ["id"])
+                        })
+                    }} />
+                }
+            }}
             fixedHeaderContent={() => {
                 return <tr>
                     <th>Id</th>
