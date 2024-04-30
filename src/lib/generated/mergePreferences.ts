62d910d44d3121bf90dd9d8675c355eaceffbc75
Victor Teo <victor.teo@fillmula.com>
2024-05-01 02:02:34 +0800

    update

--- src/lib/generated/mergePreferences.ts
+++ src/lib/generated/mergePreferences.ts
@@ -1,3 +1,6 @@
+// This file is generated and managed by Teo generator internally.
+// It will be overwritten in next generation. Do not modify this file.
+
 import { diff } from "radash"
 import defaultPreferences from "./defaultPreferences"
 import { NavItem, Preferences, SignInPreferences } from "./preferences"
