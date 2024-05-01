dcf6801f0ed9c729f8230ad4a7310871a33b3034
Victor Teo <victor.teo@fillmula.com>
2024-05-02 02:55:09 +0800

    back button shimmer

--- src/components/generated/pageStack/NavBarLeadingItems.tsx
+++ src/components/generated/pageStack/NavBarLeadingItems.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useContext } from "react"
+import React, { ReactNode, Suspense, useContext } from "react"
 import NavBarItemsContainerElement from "./NavBarItemsContainerElement"
 import { NavBarRenderStateContext } from "./NavBar"
 import StackItemIndexContext from "./stackItemIndexContext"
@@ -8,23 +8,36 @@ import { useTranslation } from "react-i18next"
 import RoundedButtonElement from "../button/RoundedButtonElement"
 import { IoIosArrowBack } from 'react-icons/io'
 import usePageStackPage from "./usePageStackPage"
+import StatusBarDefaultLeadingItemsShimmer from "../statusBar/StatusBarDefaultLeadingItemsShimmer"
+import { useAccount } from "../../../lib/generated/signIn"
 
 type NavBarLeadingItemsProps = {
     children?: ReactNode | ReactNode[]
 }
 
-const NavBarLeadingItems = ({ children }: NavBarLeadingItemsProps) => {
+type NavBarBackButtonProps = {
+    popStack: () => void
+}
+
+const NavBarBackButton = ({ popStack }: NavBarBackButtonProps) => {
     const { t } = useTranslation("translations")
+    const _ = useAccount()
+    return <WithTooltip tooltip={<Tooltip>{t("Back")}</Tooltip>}>
+        <RoundedButtonElement onClick={() => popStack()}>
+            <IoIosArrowBack />
+        </RoundedButtonElement>
+    </WithTooltip>
+}
+
+const NavBarLeadingItems = ({ children }: NavBarLeadingItemsProps) => {
     const indexContext = useContext(StackItemIndexContext)
     const navBarContext = useContext(NavBarRenderStateContext)
     const { popStack } = usePageStackPage()
     navBarContext.leading = true
     return <NavBarItemsContainerElement>
-        {indexContext.index > 0 ? <WithTooltip tooltip={<Tooltip>{t("Back")}</Tooltip>}>
-            <RoundedButtonElement onClick={() => popStack()}>
-                <IoIosArrowBack />
-            </RoundedButtonElement>
-        </WithTooltip> : null}
+        {indexContext.index > 0 ? <Suspense fallback={<StatusBarDefaultLeadingItemsShimmer />}>
+            <NavBarBackButton popStack={popStack} />
+        </Suspense> : null}
         {children}
     </NavBarItemsContainerElement>
 }
---
+++ src/components/generated/statusBar/StatusBarDefaultLeadingItemsShimmer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import RoundedButtonShimmer from '../button/RoundedButtonShimmer'
+
+const StatusBarDefaultLeadingItemsShimmer = () => <>
+    <RoundedButtonShimmer key="s1" />
+</>
+
+export default StatusBarDefaultLeadingItemsShimmer
+
