e2f15e467edb8bbe0d7b78bf4d5f5ad16136b2bd
Victor Teo <victor.teo@fillmula.com>
2024-05-05 03:12:01 +0800

    fix

--- src/components/generated/nav/NavItemShimmer.tsx
+++ src/components/generated/nav/NavItemShimmer.tsx
@@ -4,7 +4,7 @@ import Shimmer from "../shimmer/Shimmer"
 
 const NavItemShimmer = styled(Shimmer)`
     padding: 1rem;
-    height: 3rem;
+    height: calc(3rem - ${spacing});
     border-radius: ${radius};
     margin-bottom: ${spacing};
     width: 100%;
