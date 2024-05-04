36977d1757689ef774c55cdeb6c9a02dda2ae0ac
Victor Teo <victor.teo@fillmula.com>
2024-05-05 00:53:27 +0800

    fix

--- src/components/generated/shimmer/HideContentZeroOpacityElement.tsx
+++ src/components/generated/shimmer/HideContentZeroOpacityElement.tsx
@@ -2,6 +2,7 @@ import { styled } from "@linaria/react"
 
 const HideContentZeroOpacityElement = styled.div`
     opacity: 0;
+    pointer-events: none;
 `
 
 export default HideContentZeroOpacityElement
