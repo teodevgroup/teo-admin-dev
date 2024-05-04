8d16842b243128256b7b980f40d823362911b403
Victor Teo <victor.teo@fillmula.com>
2024-05-05 03:11:25 +0800

    update nav item shimmer

--- src/components/generated/nav/NavItemShimmer.tsx
+++ src/components/generated/nav/NavItemShimmer.tsx
@@ -1,11 +1,11 @@
 import { styled } from "@linaria/react"
-import { spacing } from "../../../lib/extended/theme"
+import { radius, spacing } from "../../../lib/extended/theme"
 import Shimmer from "../shimmer/Shimmer"
 
 const NavItemShimmer = styled(Shimmer)`
     padding: 1rem;
     height: 3rem;
-    border-radius: 1.5rem;
+    border-radius: ${radius};
     margin-bottom: ${spacing};
     width: 100%;
 `
