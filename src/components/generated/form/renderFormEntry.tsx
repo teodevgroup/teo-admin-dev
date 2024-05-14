4c8d8b97d9036b5392a571264407845c82d3e9dd
Victor Teo <victor.teo@fillmula.com>
2024-05-14 11:26:51 +0800

    update

--- src/components/generated/form/renderFormEntry.tsx
+++ src/components/generated/form/renderFormEntry.tsx
@@ -38,6 +38,8 @@ export type FormType = {
 const formatError = (error: ErrorOption) => {
     if (error.type === "required") {
         return "Value required"
+    } else {
+        return error.message || "Unknown error type"
     }
 }
 
